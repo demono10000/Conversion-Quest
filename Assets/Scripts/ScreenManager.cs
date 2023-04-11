1a8f2010135f50434e54504863c7122adafd0baa
demono10000 <48636182+demono10000@users.noreply.github.com>
2023-04-11 19:11:41 +0200

    added fullscreen toggle on F11

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -2,19 +2,34 @@ using UnityEngine;
 
 public class ScreenManager : MonoBehaviour
 {
-    private const float TargetAspect = 16f / 9f; // stała proporcja 16:9
+    private const float TargetAspect = 16f / 9f;
+    private bool _isFullscreen = false;
 
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
     }
-    private void FixedUpdate()
+    private void Update()
     {
         float currentAspect = (float)Screen.width / Screen.height;
         if (Mathf.Abs(currentAspect - TargetAspect) > 0.01)
         {
             float targetWidth = Screen.height * TargetAspect;
             Screen.SetResolution((int)targetWidth, Screen.height, false);
+        }
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            _isFullscreen = !_isFullscreen;
+
+            if (_isFullscreen)
+            {
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+            }
+            else
+            {
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                Screen.SetResolution(1920, 1080, false);
+            }
         }
     }
 }
